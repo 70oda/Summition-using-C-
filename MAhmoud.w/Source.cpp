1d0e99d01cf43208458cd4e4b5b7bce701e1a34c
user <user@DESKTOP-KE9RSA7>
2021-10-20 15:20:34 +0200

    Welcome Snoop

---
+++ MAhmoud.w/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int Fir, Sec;
+	cin >> Fir >> Sec;
+	cout << "Number1 + " << "Number2 = " << Fir + Sec << endl;
+
+	system("pause");
+	return 0;
+
+}
